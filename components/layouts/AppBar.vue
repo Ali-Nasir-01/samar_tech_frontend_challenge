be6de71633e0a581521c521b21bf6e1872a1a8eb
nasir <ali.nasir.0104@gmail.com>
2023-12-09 19:58:49 +0330

    feat: add svg-render global component

--- components/layouts/AppBar.vue
+++ components/layouts/AppBar.vue
@@ -1,18 +1,23 @@
 <template>
   <header class="w-full bg-[#fff] py-6 rounded-b-lg shadow">
     <div class="container mx-auto flex justify-around max-w-[550px] mt-3">
-      <NuxtLink
-        v-for="item in items"
-        :key="item.text"
-        :to="item.link"
-        :class="[item.link === path ? 'text-primary' : '']"
-      >
-        {{ item.text }}
-        <div
-          v-if="path === item.link"
-          class="bg-primary w-[8px] h-[8px] rounded-full mt-2 mx-auto"
-        ></div>
-      </NuxtLink>
+      <template v-for="item in items" :key="item.text">
+        <SvgRender
+          :icon="item.icon"
+          class="mt-1 -ml-5"
+          :color="item.link === path ? '#E20054' : undefined"
+        />
+        <NuxtLink
+          :to="item.link"
+          :class="[item.link === path ? 'text-primary' : '']"
+        >
+          {{ item.text }}
+          <div
+            v-if="path === item.link"
+            class="bg-primary w-[8px] h-[8px] rounded-full mt-2 mx-auto"
+          ></div>
+        </NuxtLink>
+      </template>
     </div>
   </header>
 </template>
@@ -25,22 +30,22 @@ const items = [
   {
     text: "لیست محصولات",
     link: "/products",
-    icon: "",
+    icon: "products",
   },
   {
     text: "دریافت مشاوره",
     link: "/#",
-    icon: "",
+    icon: "book",
   },
   {
     text: "سوالات متداول",
     link: "/#",
-    icon: "",
+    icon: "question",
   },
   {
     text: "تماس با ما",
     link: "/#",
-    icon: "",
+    icon: "phone",
   },
 ];
 </script>
