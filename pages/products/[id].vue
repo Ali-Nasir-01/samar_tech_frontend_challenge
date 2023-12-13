011c60f96b6e510a6afac7745c286977dd9d052f
nasir <ali.nasir.0104@gmail.com>
2023-12-13 07:41:19 +0330

    feat: single product breadcrumb

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,5 +1,30 @@
 <template>
-  <div></div>
+  <div>
+    <!-- BreadCrumb -->
+    <div class="flex items-center">
+      <NuxtLink to="/products">
+        <SvgRender icon="home" width="27" color="#6783A0" />
+      </NuxtLink>
+      <SvgRender
+        icon="arrow-down"
+        width="21"
+        color="#6783A0"
+        class="rotate-90 mx-4"
+      />
+      <NuxtLink class="text-[#6783A0] font-[500] text-lg" to="/products">
+        لیست محصولات
+      </NuxtLink>
+      <SvgRender
+        icon="arrow-down"
+        width="21"
+        color="#6783A0"
+        class="rotate-90 mx-4"
+      />
+      <span class="text-[#6783A0] font-[500] text-lg">
+        {{ product?.title || "-" }}
+      </span>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
